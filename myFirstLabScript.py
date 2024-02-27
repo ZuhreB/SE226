c3a529b170a45f5dbbe2d71f36d24133985495f5
ZuhreB <119894111+ZuhreB@users.noreply.github.com>
2024-02-27 14:17:15 +0300

    Create myFirstLabScript.py

---
+++ myFirstLabScript.py
@@ -0,0 +1,23 @@
+str=input("what is your name")
+print(str)
+ıd=input("What is your Student ID?")
+print("Your ID is",ıd)
+
+var1=int(input("enter var1"))
+var2=int(input("enter var2"))
+sum=var1+var2
+print("sum",sum)
+if var1>var2:
+ diff=var1-var2
+else:
+ diff=var2-var1
+print ("diff:",diff)
+
+
+lab_grade=int(input("enter lab grade"))
+midterm_grade=int(input("enter midterm grade"))
+final_grade=int(input("enter final grade"))
+score=int(lab_grade*0.25+midterm_grade*0.35+final_grade*0.40)
+print(score)
+
+print("*\n**\n***\n**\n*")
